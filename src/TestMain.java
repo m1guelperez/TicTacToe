9bbcc13638beeb9f95c139848db93bf064f1e258
Miguel Perez <35203373+FutureChanger@users.noreply.github.com>
2019-12-21 22:40:35 +0100

    Delete TestMain.java

--- src/TestMain.java
+++
@@ -1,9 +0,0 @@
-public class TestMain {
-    public static void main(String[] args) {
-        for (int i = 0; i < 50; i++) {
-            int a = 1 + (int) (Math.random() * ((3 - 1) + 1));
-            int b = 1 + (int) (Math.random() * ((3 - 1) + 1));
-            System.out.println(a + " " + b);
-        }
-    }
-}
