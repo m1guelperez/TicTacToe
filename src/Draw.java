454ec96a8dee9b99c7c1e76c737950d89fb3d21a
FutureChanger <migi.perez@web.de>
2019-12-21 22:38:53 +0100

    first commit

---
+++ src/Draw.java
@@ -0,0 +1,29 @@
+public class Draw {
+
+    //Initialize array and start board
+    public void drawIni() {
+        Storage.storageIni();
+        for (int i = 0; i < 3; i++) {
+            System.out.println(" ----- ----- -----");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|  " + Storage.store[i][j] + "  ");
+            }
+            System.out.print("|");
+            System.out.println("");
+        }
+        System.out.println(" ----- ----- -----");
+    }
+
+    //Draw the array with the updated array values entries!
+    public void draw() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(" ----- ----- -----");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|  " + Storage.store[i][j] + "  ");
+            }
+            System.out.print("|");
+            System.out.println("");
+        }
+        System.out.println(" ----- ----- -----");
+    }
+}
